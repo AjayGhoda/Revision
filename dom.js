52b4ed9708caba18d1fac77c870ca9b5af3c92fd
AjayGhoda <dinkomkar@gmail.com>
2023-06-20 16:34:46 +0530

    tried queryselectors

--- dom.js
+++ dom.js
@@ -1,18 +1,32 @@
 
-
+//initial dom exploration
 // const button = document.querySelector('.btn');
 //  button.addEventListener('click' , (e)=>{
 // e.preventDefault();
 // document.querySelector('form').classList.add('bg-dark');
 //  })
- document.getElementById('main-header').style.borderBottom = 'solid 3px Black';
-const additem = document.querySelector('h2');
-additem.style.color = 'Green';
-additem.style.fontWeight = 900;
+//  document.getElementById('main-header').style.borderBottom = 'solid 3px Black';
+// const additem = document.querySelector('h2');
+// additem.style.color = 'Green';
+// additem.style.fontWeight = 900;
 
-const items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'Green';
-for(let i =0;i<items.length ; i++){
-    items[i].style.fontWeight = 'bold';
+//used getelementsbyclassname
+// const items = document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor = 'Green';
+// for(let i =0;i<items.length ; i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+//used query selector here 
+// document.querySelector('.list-group-item:nth-child(2)').style.backgroundColor = 'green';
+// document.querySelector('.list-group-item:nth-child(3)').style.visibility = "hidden";
+
+//used query selector all here 
+const secondlistitem = document.querySelectorAll('li');
+secondlistitem[2].style.color = 'green';
+const odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i =0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'red';
 }
+
 
