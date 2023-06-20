1e090918e48858cf0cde5609c3b987de16c1a6b8
AjayGhoda <dinkomkar@gmail.com>
2023-06-20 14:32:15 +0530

    first first added to revision

--- dom.js
+++ dom.js
@@ -1,9 +1,9 @@
-const ul = document.querySelector('.items');
-ul.firstElementChild.style.background = 'green';
-ul.children[1].style.color = 'yellow';
+
 
 const button = document.querySelector('.btn');
  button.addEventListener('click' , (e)=>{
 e.preventDefault();
 document.querySelector('form').classList.add('bg-dark');
  })
+ 
+
