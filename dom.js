40e4e8f9ffdd37823b098ce88d31af6967437649
AjayGhoda <dinkomkar@gmail.com>
2023-06-20 15:40:52 +0530

    Added Color to tags and did expirementing

--- dom.js
+++ dom.js
@@ -1,9 +1,12 @@
 
 
-const button = document.querySelector('.btn');
- button.addEventListener('click' , (e)=>{
-e.preventDefault();
-document.querySelector('form').classList.add('bg-dark');
- })
- 
+// const button = document.querySelector('.btn');
+//  button.addEventListener('click' , (e)=>{
+// e.preventDefault();
+// document.querySelector('form').classList.add('bg-dark');
+//  })
+ document.getElementById('main-header').style.borderBottom = 'solid 3px Black';
+const additem = document.querySelector('h2');
+additem.style.color = 'Green';
+additem.style.fontWeight = 900;
 
