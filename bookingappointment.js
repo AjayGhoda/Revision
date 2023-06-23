08862740e1cc90ece38e0c504b8ab10557329635
AjayGhoda <dinkomkar@gmail.com>
2023-06-23 05:51:14 +0530

    store object in local storage

---
+++ bookingappointment.js
@@ -0,0 +1,10 @@
+// let form = document.querySelector('my-form');
+// form.addEventListener('onsubmit',addtolocal);
+// function addtolocal(ea){
+// ea.preventDefault();
+// let n = document.getElementById('name');
+// let e = document.getElementById('email');
+// let lname = ea.target.n.value;
+// let lemail = ea.target.e.value;
+// localStorage.setItem('name',n);
+// }
